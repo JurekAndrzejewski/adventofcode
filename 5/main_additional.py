546f82a9abc9acf6c8d81f245635e5f545aa29fe
JurekAndrzejewski <jerzy.andrzejewski00@gmail.com>
2025-12-08 22:11:25 +0100

    5 fully done

--- 5/main_additional.py
+++ 5/main_additional.py
@@ -1,58 +1,42 @@
-"""INPUT = 'test.txt'
+INPUT = 'input.txt'
 
 def read_file():
     with open(INPUT, 'r', encoding = 'utf-8') as f:
         ranges = []
-        numbers = []
-        ranges_flag = True
         for line in f.readlines():
             if line.strip() == "":
-                ranges_flag = False
-                continue
-            if ranges_flag:
-                ranges.append([int(line.strip().split('-')[0]), int(line.strip().split('-')[1])])
-            else:
-                numbers.append(int(line.strip()))
-    return ranges, numbers
-    
-def get_fresh_ingredients(ranges):
-    fresh_ingredients = []
-    new_ranges = []
-    for prop_range in ranges:
-        if not new_ranges:
-            new_ranges.append(prop_range)
-            continue
-        for exist_range in new_ranges:
-            print('prop_range | exist_range')
-            print(f'{prop_range} | {exist_range}')
-            if ((prop_range[0] > exist_range[0]) and (prop_range[1] < exist_range[1])):
-                break
-            elif ((prop_range[0] < exist_range[0]) and ((prop_range[1] < exist_range[1]) and (prop_range[1] > exist_range[0]))):
-                exist_range[0] = prop_range[0]
-                break
-            elif (((prop_range[0] > exist_range[0]) and (prop_range[0] < exist_range[1])) and (prop_range[1] > exist_range[1])):
-                exist_range[1] = prop_range[1]
-                break
-            else:
-                new_ranges.append(prop_range)
+                return ranges
+            ranges.append([int(line.strip().split('-')[0]), int(line.strip().split('-')[1])])
+    return ranges
+
+def overlap_checker(ranges):
+    print("overlap_checker")
+    new_ranges = [ranges[0]]
+    for r in ranges[1:]:
+        prev = new_ranges[-1]
+        print(prev, r)
+        if prev[1] >= r[0]:
+            print('overlap')
+            prev[1] = max(prev[1], r[1])
+        else:
+            print('no overlap')
+            new_ranges.append(r)
+        print(new_ranges)
     return new_ranges
 
-ranges, numbers = read_file()
-fresh_ingredients = get_fresh_ingredients(ranges)
+ranges = read_file()
+ranges = sorted(ranges, key=lambda x: x[0])
 
-print(fresh_ingredients)"""
+length = len(ranges)
+new_length = 0
+while True:
+    length = len(ranges)
+    ranges = overlap_checker(ranges)
+    new_length = len(ranges)
+    if length == new_length:
+        break
 
-exist_range = [5 ,10]
-prop_range = [4, 8]
-if ((prop_range[0] > exist_range[0]) and (prop_range[1] < exist_range[1])):
-    print('a')
-elif ((prop_range[0] < exist_range[0]) and ((prop_range[1] < exist_range[1]) and (prop_range[1] > exist_range[0]))):
-    exist_range[0] = prop_range[0]
-    print('b')
-elif (((prop_range[0] > exist_range[0]) and (prop_range[0] < exist_range[1])) and (prop_range[1] > exist_range[1])):
-    exist_range[1] = prop_range[1]
-    print('c')
-elif (prop_range[0] < exist_range[0]) and (prop_range[1] > exist_range[1]):
-    print('d')
-elif prop_range == exist_range:
-    print('e')
+c = 0
+for new_range in ranges:
+    c += new_range[1]-new_range[0]+1
+print(c)
